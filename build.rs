9cbace047c9a59f31d1f15eae062fea812763411
fewensa <fewensa@protonmail.com>
2019-05-05 16:21:41 +0800

    rm cc

--- build.rs
+++ build.rs
@@ -1,30 +1,30 @@
-extern crate cc;
+//extern crate cc;
 //extern crate bindgen;
 
-use std::path::PathBuf;
+//use std::path::PathBuf;
 
 fn main() {
 //  println!("cargo:rustc-link-lib=static=stdc++");
 //  println!("cargo:rustc-link-lib=gslcblas");
 
 
-  cc::Build::new()
-    .cpp(true)
-    .file("rsnowboywrapper/rsnowboy.cpp")
-    .include("rsnowboywrapper")
-    .flag("-c")
-    .flag("-lstdc++")
-    .flag("-D_GLIBCXX_USE_CXX11_ABI=0")
-    .flag("-fPIC")
-    .flag("-std=c++0x")
-    .flag("-Wall")
-    .flag("-Wno-sign-compare")
-    .flag("-Wno-unused-local-typedefs")
-    .flag("-Winit-self")
-    .flag("-rdynamic")
-    .flag("-DHAVE_POSIX_MEMALIGN")
-    .flag("-O3")
-    .compile("librsnowboywrapper.a");
+//  cc::Build::new()
+//    .cpp(true)
+//    .file("rsnowboywrapper/rsnowboy.cpp")
+//    .include("rsnowboywrapper")
+//    .flag("-c")
+//    .flag("-lstdc++")
+//    .flag("-D_GLIBCXX_USE_CXX11_ABI=0")
+//    .flag("-fPIC")
+//    .flag("-std=c++0x")
+//    .flag("-Wall")
+//    .flag("-Wno-sign-compare")
+//    .flag("-Wno-unused-local-typedefs")
+//    .flag("-Winit-self")
+//    .flag("-rdynamic")
+//    .flag("-DHAVE_POSIX_MEMALIGN")
+//    .flag("-O3")
+//    .compile("librsnowboywrapper.a");
 
 
 //  println!("cargo:rustc-link-lib=bz2");
